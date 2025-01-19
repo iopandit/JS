3f3a66d6baa6e69c6e9b0881ad91d80e889220ba
iopandit <156647017+iopandit@users.noreply.github.com>
2025-01-19 15:23:58 +0000

    add variableFile

---
+++ CACode/01Basics/variables.js
@@ -0,0 +1,10 @@
+let name = "Ishwar";
+const account_no = 1234567;
+var account_id = 3;
+accountCity = "Latur"
+
+console.log(name);
+console.log(accountCity); //possible to declare without using let,const,var
+
+
+console.table([name,account_id,account_no])
