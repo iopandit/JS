a4bf2da81495bbcf4e003deee4705a677d6042de
iopandit <156647017+iopandit@users.noreply.github.com>
2025-01-20 12:46:34 +0000

    added  conversion opration on number

---
+++ CACode/01Basics/variableConversion.js
@@ -0,0 +1,15 @@
+// let age = 22;
+// let isNumber = Number(age)
+// console.log(isNumber) //22
+// console.log(typeof isNumber); //number
+
+// age ="22";
+// let isNumber = Number(age)
+// console.log(isNumber) //22
+// console.log(typeof isNumber); //number
+
+let age = "22abf"
+let isNumber = Number(age)
+console.log(isNumber) //Nan
+console.log(typeof isNumber); //number
+
