c1fe5d603279bd6c2d8a67a0b886da03ba6b4152
iopandit <156647017+iopandit@users.noreply.github.com>
2025-03-02 14:39:47 +0000

    conversion of data types

--- CACode/01Basics/variableConversion.js
+++ CACode/01Basics/variableConversion.js
@@ -8,8 +8,23 @@
 // console.log(isNumber) //22
 // console.log(typeof isNumber); //number
 
-let age = "22abf"
-let isNumber = Number(age)
-console.log(isNumber) //Nan
-console.log(typeof isNumber); //number
+// let age = "22abf"
+// let isNumber = Number(age)
+// console.log(isNumber) //Nan
+// console.log(typeof isNumber); //number
+
+// let age = null
+// let isNumber = Number(age)
+// console.log(isNumber)  //0
+// console.log(typeof isNumber);  //number
+
+// let age = undefined
+// let isNumber = Number(age)
+// console.log(isNumber) //nan
+// console.log(typeof isNumber); //number
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber); //33
+console.log(typeof(stringNumber)); //string
 
