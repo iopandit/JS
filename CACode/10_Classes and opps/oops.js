e7372c8a9e0f47a2852bbae7b702ddf44552f889
iopandit <156647017+iopandit@users.noreply.github.com>
2025-03-20 12:56:50 +0530

    opps

---
+++ CACode/10_Classes and opps/oops.js
@@ -0,0 +1,12 @@
+const  user ={
+    userName = "Ishwar",
+    loginCount = 5,
+    isSignedIn= true,
+
+    getUserDetails : function(){
+        console.log("Got user details from DB");
+        
+    }
+}
+
+console.log(user.userName);
